35eec83eb7f8e9eb5086236a6d6ae8c45dff6ecf
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-05-24 10:28:41 +0000

    M package.json  M src/index.ts  M tsconfig.json  M yarn.lock ?? src/createRouter.ts

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,16 @@
-const defaultOptions: Options = { errorHandler() {}, middlewares: [] }
-export function createRouter(options = {} as Partial<Options>) {
-  const conf = { ...defaultOptions, ...options }
+import { ErrorManager } from 'req-error-core'
+import createRouter from './createRouter'
 
-  return function (routes: Partial<RouteOptions>) {
-    return (req: any, res: any) => {
-      const methodHandlers = (routes as any)[req.method!.toLowerCase()]
-      const isHandlerAvailable = Array.isArray(methodHandlers)
-        ? Boolean(methodHandlers.length)
-        : typeof methodHandlers === 'function'
+export * from 'req-error-core'
+export * from './createRouter'
 
-      const rawHandler = isHandlerAvailable ? methodHandlers : routes.all
-      if (!rawHandler) return
-
-      const handlers = Array.isArray(rawHandler) ? rawHandler : [rawHandler]
-      const allHandlers = [...conf.middlewares, ...handlers]
-
-      const newList = allHandlers.map((handler, ind) => {
-        return async () => {
-          const nextFn = newList[ind + 1]
-
-          try {
-            const rv = handler(req, res, () => nextFn && nextFn())
-            if (rv instanceof Promise) await rv
-          } catch (err) {
-            conf.errorHandler(err, req, res)
-          }
-        }
-      })
-
-      const firstFn = newList[0]
-      firstFn && firstFn()
-    }
-  }
-}
-
-export default createRouter({
-  errorHandler(err, req, res) {
-    res.status(400).json({
-      status: 'fail',
-      message: err.message,
-    })
-  },
-})
-
-type RouterItem = Function | Function[]
-interface RouteOptions {
-  all: RouterItem
-  get: RouterItem
-  put: RouterItem
-  post: RouterItem
-  head: RouterItem
-  patch: RouterItem
-  trace: RouterItem
-  delete: RouterItem
-  connect: RouterItem
-  options: RouterItem
-}
-
-interface Options {
-  errorHandler(err: any, req: any, res: any): void
-  middlewares: Function[]
-}
+export default (() => {
+  const errorManager = new ErrorManager()
+  return createRouter({
+    errorHandler(err, req, res) {
+      const [message, statusCode] = errorManager.getErrorInfo(err)
+      const status = statusCode < 500 ? 'fail' : 'error'
+      res.status(statusCode).json({ status, message })
+    },
+  })
+})()
